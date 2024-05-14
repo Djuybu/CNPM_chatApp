1b176a1c10f72d75e44d0b5da9a19119a0a6a597
Trung <22028170@vnu.edu.vn>
2024-05-14 08:20:14 +0700

    LOGIN CHECK

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,11 +1,24 @@
 import "./login.css"
+import {useState} from "react";
 
 const Login= () =>
 {
+    const [avatar,setAvatar]=useState({
+        file:null,
+        url:""
+    });
+    const handleAvatar = e =>{
+        if(e.target.files[0]) {
+            setAvatar({
+                file: e.target.files[0],
+                url: URL.createObjectURL(e.target.files[0])
+            })
+        }
+    }
     return(
         <div className="login">
             <div className="item">
-                <h2>Xin Chào</h2>
+                <h2>ĐĂNG NHẬP</h2>
                 <form>
                     <input type="text" placeholder="Email"/>
                     <input type="password" placeholder="Password"/>
@@ -13,16 +26,20 @@ const Login= () =>
                 </form>
             </div>
             <div className="separator"></div>
-            <div className="item"></div>
-            <h2>Tạo Tài Khoản Mới</h2>
+            <div className="item">
+            <h2>TẠO TÀI KHOẢN MỚI</h2>
             <form>
-                <input type="file"  id="file"/>
+                <label htmlFor="file" className="label-avatar">
+                    <img src={avatar.url || "public/avatar.png"} alt={""}/>
+                    Chon Anh Dai Dien
+                </label>
+                <input type="file" id="file" style={{display: "none"}} onChange={handleAvatar}/>
                 <input type="text" placeholder="Username" name="username"/>
                 <input type="text" placeholder="Email"/>
                 <input type="password" placeholder="Password"/>
                 <button>Đăng Kí</button>
             </form>
-
+            </div>
 
         </div>
     )
