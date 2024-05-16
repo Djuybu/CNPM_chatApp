cad6fd76061fd67d6114857b207f581878a84e3b
Trung <22028170@vnu.edu.vn>
2024-05-16 22:04:43 +0700

    deploy 16/5

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,137 @@
+import { useState } from "react";
+import "./login.css";
+import { toast } from "react-toastify";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth, db } from "../../lib/firebase";
+import {collection, doc, getDocs, query, setDoc, where} from "firebase/firestore";
+import upload from "../../lib/upload";
+
+const Login = () => {
+  const [avatar, setAvatar] = useState({
+    file: null,
+    url: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleAvatar = (e) => {
+    if (e.target.files[0]) {
+      setAvatar({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
+
+ const handleRegister = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const formData = new FormData(e.target);
+
+    const { username, email, password } = Object.fromEntries(formData);
+
+    // VALIDATE INPUTS
+    if (!username || !email || !password)
+      return toast.warn("Hãy điền đầy đủ thông tin!");
+    if (!avatar.file) {
+      toast.warn("Hãy chọn ảnh đại diện");
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+      return;
+    }
+
+    // VALIDATE UNIQUE USERNAME
+    const usersRef = collection(db, "users");
+    const q = query(usersRef, where("username", "==", username));
+    const querySnapshot = await getDocs(q);
+    if (!querySnapshot.empty) {
+      return toast.warn("Username đã tồn tại");
+    }
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const imgUrl = await upload(avatar.file);
+
+      await setDoc(doc(db, "users", res.user.uid), {
+        username,
+        email,
+        avatar: imgUrl,
+        id: res.user.uid,
+        blocked: [],
+      });
+
+      await setDoc(doc(db, "userchats", res.user.uid), {
+        chats: [],
+      });
+
+      toast.success("Tạo tài khoản thành công");
+        window.location.reload();
+
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const formData = new FormData(e.target);
+    const { email, password } = Object.fromEntries(formData);
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      
+      toast.success("Đăng Nhập Thành Công");
+                  window.location.reload();
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="item">
+        <h2>FLEXI CHAT WELCOME BACK</h2>
+        <form onSubmit={handleLogin}>
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button disabled={loading}>{loading ? "Loading" : "Đăng Nhập"}</button>
+        </form>
+      </div>
+      <div className="separator"></div>
+      <div className="item">
+        <h2>Đăng Nhập</h2>
+        <form onSubmit={handleRegister}>
+           <label htmlFor="file" className="label-avatar">
+                    <img src={avatar.url || "./avatar.png"} alt={""}/>
+                    Chọn Ảnh Đại Diện
+                </label>
+          <input
+            type="file"
+            id="file"
+            style={{ display: "none" }}
+            onChange={handleAvatar}
+          />
+          <input type="text" placeholder="Username" name="username" />
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button disabled={loading}>{loading ? "Loading" : "Đăng Kí"}</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
